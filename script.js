d3ef5248017360e7061514c3f49067ee72387cb0
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 11:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	get speciesNames(){
+		return this.species;
+	} 
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log(`woof`);
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log(`purr`);
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
